a576cc12e134818e867400de3530697b5aefb1c1
Ireanna <lion_iricha@tut.by>
2023-05-22 00:12:33 +0300

    lesson_4

---
+++ Lesson_4/Task_25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double myPow (double a, int n) {
+    double aN = a;
+    for (int i=1; i<n; i++) {
+        aN *= a; 
+    }
+    return aN;
+}
+
+Console.Write("Введите число, возводимое в степень: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение степени: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>0) {
+    Console.Write( $"{a} в степени {n} равно {myPow(a,n)}");
+} else 
+    if (n==0) {
+        Console.Write( $"{a} в степени {n} равно 1");
+    } else {
+        Console.Write( $"{a} в степени {n} равно {1/myPow(a,n*(-1))}");
+    }   
---
+++ Lesson_4/Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] randArray(){
+    int n = new Random().Next(1,9);
+    int[] rndArray = new int [n];
+    for(int i = 0; i < rndArray.Length;i++){
+        rndArray[i] = new Random().Next(0,100);
+        }
+    return rndArray;
+}
+
+Console.WriteLine("[" + String.Join(",", randArray())+ "]");
+
